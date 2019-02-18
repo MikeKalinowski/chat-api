8f683e4808a8d4ff424f2559630867b795c29cc2
MikeKalinowski <MikeKalinowski@users.noreply.github.com>
2019-02-18 17:07:34 +0100

    server side events

--- controllers/messagesList.js
+++ controllers/messagesList.js
@@ -1,5 +1,5 @@
 const handleMessagesList = (req, res, knex) => {
-  knex.select('id', 'content', 'userId', 'channelId')
+  knex.select('id', 'content', 'userId', 'channelId', 'date')
   .from('messages')
   .where({channelId: req.body.channelId,})
   .then(
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const bcrypt = require('bcrypt-nodejs');
+const EventEmitter = require('events');
 const knex = require('knex')({
   client: 'pg',
   connection: {
@@ -18,23 +19,30 @@ const channelsList = require('./controllers/channelsList')
 const messagesList = require('./controllers/messagesList')
 const sendMessage = require('./controllers/sendMessage')
 const app = express()
+const Stream = new EventEmitter();
 
 app.use(cors())
 app.use(bodyParser.json());
-
-app.get('/', function (req, res) {
-	knex.select('*').from('users')
-	.then(console.log)
-  .then(res.send('hello world'))
-})
-
 
 app.post('/register', (req, res) => {register.handleRegister(req, res, knex, bcrypt)})
 app.post('/login', (req, res) => {login.handleLogin(req, res, knex, bcrypt)})
 app.post('/createChannel', (req, res) => {createChannel.handleCreate(req, res, knex)})
 app.get('/channelsList', (req, res) => {channelsList.handleChannelsList(req, res, knex)})
-app.post('/messagesList', (req, res) => {messagesList.handleMessagesList(req, res, knex)})
+app.post('/messagesList', (req, res) => {Stream.emit("push", {test: "yes", tt: "no"}); messagesList.handleMessagesList(req, res, knex)})
 app.post('/sendMessage', (req, res) => {sendMessage.handleSendMessage(req, res, knex)})
 
+app.get('/messagesListSSE', function(request, response){
+  response.writeHead(200, {
+    'Content-Type': 'text/event-stream',
+    'Cache-Control': 'no-cache',
+    'Connection': 'keep-alive'
+  });
+
+  Stream.on("push", function(data) {
+    setInterval(function() {
+      response.write("data: " + JSON.stringify(data) + "\n\n");
+    }, 4000);
+  });
+});
 
 app.listen(8000) 
